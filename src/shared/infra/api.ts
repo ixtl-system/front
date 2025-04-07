19a752205c93f6cc999e1a8b6943471f2761c754
LeonardoMessias98 <messiasleonardo98@gmail.com>
2025-04-06 23:12:20 -0300

    feat: adding base url env

--- src/shared/infra/api.ts
+++ src/shared/infra/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { errorInterceptor } from "./interceptors/errorInterceptor";
 
 export const api = axios.create({
-  baseURL: "http://localhost:3333",
+  baseURL: import.meta.env.VITE_BASE_URL,
 });
 
 // api.interceptors.response.use(function (config) {
