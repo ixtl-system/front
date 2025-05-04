f5f6260cc2dbe3a86b08d1c7c51048d2ce6f89dd
LeonardoMessias98 <messiasleonardo98@gmail.com>
2025-05-04 13:37:53 -0300

    refactor: clean up commented-out code in api and errorInterceptor files

--- src/shared/infra/api.ts
+++ src/shared/infra/api.ts
@@ -5,15 +5,4 @@ export const api = axios.create({
   baseURL: import.meta.env.VITE_BASE_URL,
 });
 
-// api.interceptors.response.use(function (config) {
-//   const token = localStorage.getItem("token");
-//   if (token) {
-//     config.headers.Authorization = `Bearer ${token}`;
-//   } else {
-//     window.alert("Sem token");
-//   }
-//   return config;
-// });
-
-
 api.interceptors.response.use(config => config, errorInterceptor);
