e63d227daf0a1508e19133dde04a5c903edc3809
LeonardoMessias <leonardo@yetz.com.br>
2025-05-05 09:43:37 -0300

    refactor: reorganize imports and enhance Vite configuration structure

--- src/shared/infra/api.ts
+++ src/shared/infra/api.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+
 import { errorInterceptor } from "./interceptors/errorInterceptor";
 
 export const api = axios.create({
--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,28 @@
 import path from "node:path";
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "/src"),
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+
+export default defineConfig(() => {
+  const metaEnv: any = {
+    VITE_BASE_URL: process.env.VITE_BASE_URL
+  };
+
+  const define: { [key: string]: string } = {};
+  for (const key in metaEnv) {
+    if (Object.prototype.hasOwnProperty.call(metaEnv, key)) {
+      const data = metaEnv[key];
+      define[`process.env.${key}`] = JSON.stringify(data);
+    }
+  }
+
+  return {
+    plugins: [react()],
+    define,
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "/src"),
+      },
     },
-  },
+  }
 });
